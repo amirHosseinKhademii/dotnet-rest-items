d1e9e70ee50f4fb7ffb6c5fe2129714d18c4355b
amirHosseinKhademii <sourena2575@gmail.com>
2022-06-03 22:00:43 +0200

    Add mongo db credential via .net secret manager

--- obj/Debug/net6.0/Catalog.AssemblyInfo.cs
+++ obj/Debug/net6.0/Catalog.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("a25b7f40-ba05-43a3-9f85-9f5250a373fa")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
